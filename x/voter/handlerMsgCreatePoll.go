64b8e2ee4a6b2226e7095ad5c27dbda28bd6ff18
Ilia Bukalov <ilyabukalov@gmail.com>
2020-09-29 10:01:37 +0800

    token couing in handler

--- x/voter/handlerMsgCreatePoll.go
+++ x/voter/handlerMsgCreatePoll.go
@@ -3,6 +3,7 @@ package voter
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ilyabukalov/voter/x/voter/types"
+	"github.com/tendermint/tendermint/crypto"
 )
 
 func handleMsgCreatePoll(ctx sdk.Context, k Keeper, msg MsgCreatePoll) (*sdk.Result, error) {
@@ -12,6 +13,12 @@ func handleMsgCreatePoll(ctx sdk.Context, k Keeper, msg MsgCreatePoll) (*sdk.Res
     Title: msg.Title,
     Options: msg.Options,
 	}
+	moduleAcct := sdk.AccAddress(crypto.AddressHash([]byte(types.ModuleName)))
+	payment, _ := sdk.ParseCoins("200token")
+	if err := k.CoinKeeper.SendCoins(ctx, poll.Creator, moduleAcct, payment); err != nil {
+		return nil, err
+	}
+
 	k.CreatePoll(ctx, poll)
 
 	return &sdk.Result{Events: ctx.EventManager().Events()}, nil
